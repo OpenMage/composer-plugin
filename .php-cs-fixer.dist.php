1d27a89b3f61c350b0d383c08d69202dd6f6520b
Sven Reichel <github-sr@hotmail.com>
2025-01-13 17:26:30 +0100

    updated php-cs-config

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -9,14 +9,16 @@ return $config
         'modernize_types_casting' => true,
         'nullable_type_declaration_for_default_null_value' => true,
         'single_quote' => true,
+        'php_unit_test_case_static_method_calls' => true,
         'trailing_comma_in_multiline' => ['after_heredoc' => true, 'elements' => ['arguments', 'array_destructuring', 'arrays']],
     ])
     ->setFinder(
         PhpCsFixer\Finder::create()
             ->in([
                 __DIR__ . '/src',
+                __DIR__ . '/tests',
             ])
             ->name('*.php')
             ->ignoreDotFiles(true)
-            ->ignoreVCS(true)
+            ->ignoreVCS(true),
     );
